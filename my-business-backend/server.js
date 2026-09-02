10dca2e7f9b5ec18da0194d949ffdd44ccaeb329
Edezio Masina <bsc-36-23@unima.ac.mw>
2026-09-02 14:53:51 +0200

    Prepare backend for Render deployment

--- my-business-backend/server.js
+++ my-business-backend/server.js
@@ -1510,7 +1510,7 @@ const PORT =
     process.env.PORT || 3000;
 
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
 
     console.log(
         `EM-PLUGS Business Inventory API running on port ${PORT}`
